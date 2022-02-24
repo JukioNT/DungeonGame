74b27a99387ab9e355d8e4a11f9727c9a61de2c6
Júlio Neto <jjulio2468@gmail.com>
2022-02-24 10:22:51 -0300

    Chest drop item system
    Chest now drop just a small health potion, but will drop more items on future

--- DungeonGame/Assets/Scripts/Chests/ChestDropItems.cs
+++ DungeonGame/Assets/Scripts/Chests/ChestDropItems.cs
@@ -6,17 +6,30 @@ public class ChestDropItems : MonoBehaviour
 {
 
     private OpenChest chest;
+    private float chestPosX;
+    private float chestPosY;
+
+    //GameItems
+    private GameObject smallHPotion;
 
     void Start()
     {
         chest = gameObject.GetComponent<OpenChest>();
+        chestPosX = chest.GetComponent<Transform>().position.x;
+        chestPosY = chest.GetComponent<Transform>().position.y;
+        GetItems();
     }
 
     void Update()
     {
         if (Input.GetKeyDown("e") && chest.colPlayer == true)
         {
-            
+            Instantiate(smallHPotion, new Vector3(chestPosX, chestPosY, 0), Quaternion.identity);
         }
+    }
+
+    void GetItems()
+    {
+        smallHPotion = GameObject.Find("SmallHealthPotion");
     }
 }
