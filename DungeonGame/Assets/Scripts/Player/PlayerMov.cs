778bc9dcd5464ffa9a0423a74c640d0846c895a8
JukioNT <jjulio2468@gmail.com>
2022-08-05 15:35:27 -0300

    Completed hold gun system

--- DungeonGame/Assets/Scripts/Player/PlayerMov.cs
+++ DungeonGame/Assets/Scripts/Player/PlayerMov.cs
@@ -25,19 +25,6 @@ public class PlayerMov : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown("a") || Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            sr.flipX = true;
-            weapon.GetComponent<SpriteRenderer>().flipX = true;
-            weapon.transform.position = new Vector3(gameObject.transform.position.x - 0.3f, gameObject.transform.position.y - 0.55f, gameObject.transform.position.z);
-        }
-        if (Input.GetKeyDown("d") || Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            sr.flipX = false;
-            weapon.GetComponent<SpriteRenderer>().flipX = false;
-            weapon.transform.position = new Vector3(gameObject.transform.position.x + 0.3f, gameObject.transform.position.y - 0.55f, gameObject.transform.position.z);
-        }
-
         if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
         {
             movAnimation.SetBool("isRunning", true);
@@ -55,5 +42,21 @@ public class PlayerMov : MonoBehaviour
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(directionMov.x * moveSpeed, directionMov.y * moveSpeed);
+    }
+
+    public void FlipPlayer(bool flip)
+    {
+        if (flip == false)
+        {
+            sr.flipX = true;
+            weapon.GetComponent<SpriteRenderer>().flipX = true;
+            weapon.transform.position = new Vector3(gameObject.transform.position.x - 0.3f, gameObject.transform.position.y - 0.55f, gameObject.transform.position.z);
+        }
+        else
+        {
+            sr.flipX = false;
+            weapon.GetComponent<SpriteRenderer>().flipX = false;
+            weapon.transform.position = new Vector3(gameObject.transform.position.x + 0.3f, gameObject.transform.position.y - 0.55f, gameObject.transform.position.z);
+        }
     }
 }
--- DungeonGame/Assets/Scripts/Player/PointToCursor.cs
+++ DungeonGame/Assets/Scripts/Player/PointToCursor.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class PointToCursor : MonoBehaviour
 {
+
+    private PlayerMov flip;
+    private GameObject player;
+
+    void Start()
+    {
+        flip = Object.FindObjectOfType<PlayerMov>();
+        player = GameObject.Find("Player");
+    }
+
     void FixedUpdate()
     {
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         mousePos.Normalize();
-
         float rotationz = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
-
         transform.rotation = Quaternion.Euler(0f, 0f, rotationz-90);
+
+        Vector3 mousePosPlayer = Camera.main.ScreenToWorldPoint(Input.mousePosition) - player.transform.position;
+
+        if (mousePosPlayer.x > 0)
+        {
+            flip.FlipPlayer(true);
+        }
+        else
+        {
+            flip.FlipPlayer(false);
+        }
     }
 }
